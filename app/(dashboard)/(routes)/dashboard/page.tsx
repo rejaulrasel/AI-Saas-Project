880be9cac640053773529a5c86c434629e466057
rejaul rasel <rejaulhasanrasel28@gmail.com>
2024-01-17 15:03:57 +0600

    added sidebar for medium and large devices

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,7 @@
-import { Button } from '@/components/ui/button'
-import { UserButton } from '@clerk/nextjs';
-
 const DashboardPage = () => {
   return (
     <div>
-    <p>Dashboard Page</p>
-    <UserButton afterSignOutUrl='/'/>
+    <p>Dashboard Page (Protected)</p>
 
     </div>
     )
