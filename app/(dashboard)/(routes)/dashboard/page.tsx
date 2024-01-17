3a5ed44435dd7fb15375b27d0a613fcd4bc0bba7
rejaul rasel <rejaulhasanrasel28@gmail.com>
2024-01-17 12:26:05 +0600

    added Authentication system

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,13 @@
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs';
 
 const DashboardPage = () => {
   return (
+    <div>
     <p>Dashboard Page</p>
+    <UserButton afterSignOutUrl='/'/>
+
+    </div>
     )
 }
 
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const page = () => {
     return (
         <div>
             Landing page (Unprotected)
+
+            <div>
+                <Link href='/sign-in'>
+                    <Button>Login</Button>
+                </Link>
+                <Link href='/sign-up'>
+                    <Button>Register</Button>
+                </Link>
+            </div>
         </div>
     );
 };
